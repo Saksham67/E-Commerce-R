bdb550e1f6337c23354869177d7010caad224116
Shahana700 <shahanacs2003@gmail.com>
2025-09-11 15:44:18 +0530

    node setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const mongoose = require("mongoose");
 
 
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/";
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log("Connected to the DB"))
+  .catch(err => console.error("DB connection error:", err));
+
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error:', err);
+});
 
 
 app.get("/" , (req,res) =>{
